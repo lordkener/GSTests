8065902963f1e8c434a0fff2ed86e1fd3cde1e31
lordkener <genioinstrumental@gmail.com>
2020-12-02 03:39:20 +0100

    Creating react project

---
+++ react/frontend/phonecatalog/src/components/Main.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+class Main extends Component{
+    render(){
+        return <h1>Hey, i miss you React :D it's me! Hamsa!</h1>
+    }
+}
+export default Main;
